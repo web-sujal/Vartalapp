b33fb7d803f7cd3d050eb7e0ed3ffde97f3d8528
Sujal <web.sujal@gmail.com>
2023-12-08 13:43:58 +0530

    created message component

--- src/components/ChatDetails.tsx
+++ src/components/ChatDetails.tsx
@@ -3,6 +3,7 @@ import { mockChatList } from "../mockData/mockChatList";
 import Avatar from "./Avatar";
 import { HiOutlinePhone } from "react-icons/hi";
 import { FaVideo } from "react-icons/fa";
+import Message from "./Message";
 
 const ChatDetails = () => {
   const location = useLocation();
@@ -51,8 +52,56 @@ const ChatDetails = () => {
       </div>
 
       {/* chat deatails section */}
-      <div className="flex h-full items-start justify-start gap-4 rounded-2xl bg-white p-4 dark:bg-gray-800 dark:text-white">
-        chat details
+      <div className="flex h-full max-h-full flex-col items-start justify-start gap-1 overflow-y-auto rounded-2xl bg-white p-4 dark:bg-gray-800 dark:text-white [&::-webkit-scrollbar]:w-0">
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="hello joshi ji"
+        />
+        <Message timestamp="12:37 AM" message="Ji btaiye" />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="Kaise hain aap..."
+        />
+        <Message timestamp="12:37 AM" message="theek hun" />
+        {/* <Message timestamp="12:37 AM" message="aap sunao..." />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="hum bhi badiya hai"
+        />
+        <Message timestamp="12:37 AM" message="ok" />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="wise man say only fools rush in, and I can't help falling in love with you I wonder if I'm ever on her mind like, Probably not swear I've never seen a girl so fine"
+        />
+        <Message timestamp="12:37 AM" message="theek baa" />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="hello joshi ji"
+        />
+        <Message timestamp="12:37 AM" message="Ji btaiye" />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="Kaise hain aap..."
+        />
+        <Message timestamp="12:37 AM" message="theek baa" />
+        <Message timestamp="12:37 AM" message="aap sunao..." />
+        <Message
+          timestamp="12:37 AM"
+          isCurrentuser={true}
+          message="hum bhi badiya hai"
+        />
+        <Message timestamp="12:37 AM" message="ok" />
+        <Message
+          timestamp="12:37 AM"
+          message="wise man say only fools rush in, and I can't help falling in love with you I wonder if I'm ever on her mind like, Probably not swear I've never seen a girl so fine"
+        />
+        <Message timestamp="12:37 AM" message="theek baa" /> */}
       </div>
     </div>
   );
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,34 @@
-const Message = () => {
-  return <div>Message</div>;
+type MessageProps = {
+  isCurrentuser?: boolean;
+  message: string;
+  timestamp: string;
+};
+
+const Message = ({
+  isCurrentuser = false,
+  message,
+  timestamp,
+}: MessageProps) => {
+  return (
+    <div
+      className={`${
+        isCurrentuser
+          ? "ms-auto bg-rose-600"
+          : "me-auto bg-gray-200 text-black dark:bg-slate-600 dark:text-white "
+      } flex w-4/5 max-w-fit flex-col items-start justify-center rounded-3xl px-4 py-2 text-lg text-white shadow-sm`}
+    >
+      <div className="">{message}</div>
+      <div
+        className={`${
+          isCurrentuser
+            ? "text-gray-50 dark:text-white/90"
+            : " text-gray-700 dark:text-gray-300"
+        } w-full text-end text-xs font-light tracking-tight`}
+      >
+        {timestamp}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
